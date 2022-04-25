4100c198a6e9f3de4406b1c27ce0bd1251ebc765
Samarth <Varahi1917115@gmail.com>
2022-04-25 18:17:57 -0400

    commit tree 4c225bcf830e0b45b96b190e2c13d97220cca252

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
 
+const bookPath =  "https://anapioficeandfire.com/api/books"
+function getResponse(response)
+{ return response.json() }
+function processJSON(json)
+{
+  for(let i = 0; i < json.length; i++)
+  {
+    console.log(json[i])
+  }
+}
+function fetchBooks() {
+  return fetch(bookPath)
+  .then(getResponse)
+  .then(processJSON);
+  
+};
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
+    
   });
 }
-
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  
+  fetchBooks()
 });
